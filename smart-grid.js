7bb21806ff3775e690f9f598687901711674b7fe
dimonabinsk <dimonabinsk@gmail.com>
2021-08-11 21:51:51 +0300

    Сверстал шапку, заменил иконки свг на иконочный шрифт

--- smart-grid.js
+++ smart-grid.js
@@ -5,26 +5,28 @@ const settings = {
     offset: '20px', /* gutter width px || % || rem */
     mobileFirst: false,
     container: {
-        maxWidth: '1200px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
+        maxWidth: '1440px', /* max-width оn very large screen */
+        fields: '120px', /* side fields боковые поля */
     },
     breakPoints: {
         
         lg: {
             width: '1100px', /* -> @media (max-width: 1100px) */
+            fields: '60px',
         },
         md: {
-            width: '960px'
+            width: '960px',
+            fields: '30px',
         },
         sm: {
             width: '780px',
-            fields: '15px' /* set fields only if you want to change container.fields */
+            fields: '15px', /* set fields only if you want to change container.fields */
         },
         xs: {
-            width: '560px'
+            width: '560px',
         },
         phone: {
-            width: '420px', /* -> @media (max-width: 420px) */
+            width: '320px', /* -> @media (max-width: 420px) */
         },
     }
 };
